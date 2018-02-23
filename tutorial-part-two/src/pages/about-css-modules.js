d79b5d847b778a2c64ddac81ffc1c3a4426384a6
serhii.l@itua.com.ua <b13579GL>
2018-02-23 19:25:17 +0200

    part two of tutorial

---
+++ tutorial-part-two/src/pages/about-css-modules.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Container from "../components/container";
+
+export default () => (
+    <Container>
+        <h1>About Css Modules</h1>
+        <p>Css Modules are Cool</p>
+    </Container>
+);
